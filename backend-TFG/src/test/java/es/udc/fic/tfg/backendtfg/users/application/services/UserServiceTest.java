e6fe424ebad60abc7019031c46d50cc7e3f387e3
Javier Cancela Mato <javier.cmato@udc.es>
2022-06-15 10:21:43 +0200

    Tests de servicio para iniciar sesión
    Creado sonar-project.properties global para todo el proyecto (backend y frontend)

--- backend-TFG/src/test/java/es/udc/fic/tfg/backendtfg/users/application/services/UserServiceTest.java
+++ backend-TFG/src/test/java/es/udc/fic/tfg/backendtfg/users/application/services/UserServiceTest.java
@@ -1,8 +1,9 @@
 package es.udc.fic.tfg.backendtfg.users.application.services;
 
-import es.udc.fic.tfg.backendtfg.common.domain.exceptions.EntityAlreadyExistsException;
+import es.udc.fic.tfg.backendtfg.common.domain.exceptions.*;
 import es.udc.fic.tfg.backendtfg.users.domain.entities.User;
 import es.udc.fic.tfg.backendtfg.users.domain.entities.UserRole;
+import es.udc.fic.tfg.backendtfg.users.domain.exceptions.IncorrectLoginException;
 import es.udc.fic.tfg.backendtfg.users.domain.repositories.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,6 +63,7 @@ class UserServiceTest {
         assertEquals(registeredUser, databaseUser);
     }
     
+    
     @Test
     void whenSigningUpTwice_thenUserAlreadyExists( ) throws EntityAlreadyExistsException {
         // Crear datos de prueba
@@ -79,6 +81,177 @@ class UserServiceTest {
                 ( ) -> assertThrows(
                         EntityAlreadyExistsException.class,
                         ( ) -> userService.signUp(expectedUser)
+                )
+        );
+    }
+    
+    
+    @Test
+    void whenLogin_thenUserDataIsReturned()
+            throws EntityAlreadyExistsException, IncorrectLoginException, ResourceBannedByAdministratorException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+        
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+        User loggedUser = userService.login(nickname, clearPassword);
+        
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Se puede recuperar los datos del usuario
+                () -> assertEquals(expectedUser.getNickname(), loggedUser.getNickname()),
+                () -> assertNotNull(expectedUser.getAvatar()),
+                () -> assertEquals(UserRole.USER, expectedUser.getRole())
+        );
+    }
+    
+    
+    @Test
+    void whenLoginWithIncorrectNickname_thenRaiseIncorretLoginException() throws EntityAlreadyExistsException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+        
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+        
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Se lanza la excepción
+                () -> assertThrows(
+                        IncorrectLoginException.class,
+                        () -> userService.login(nickname + 'X', clearPassword)
+                )
+        );
+    }
+    
+    
+    @Test
+    void whenLoginWithIncorrectPassword_thenRaiseIncorretLoginException() throws EntityAlreadyExistsException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+    
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+        
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Se lanza la excepción
+                () -> assertThrows(
+                        IncorrectLoginException.class,
+                        () -> userService.login(nickname, clearPassword + 'X')
+                )
+        );
+    }
+    
+    
+    @Test
+    void whenLoginNonExistentUser_thenRaiseIncorrectLoginException() {
+        // Comprobar resultados
+        assertAll(
+                // Se lanza la excepción
+                () -> assertThrows(
+                        IncorrectLoginException.class,
+                        () -> userService.login(NON_EXISTENT_NICKNAME, DEFAULT_PASSWORD)
+                )
+        );
+    }
+    
+    
+    @Test
+    void whenUserIsBanned_andLogin_thenRaiseResourceBannedByAdminException() throws EntityAlreadyExistsException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+        expectedUser.setBannedByAdmin(true);
+    
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+    
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Usuario está baneado
+                () -> assertTrue(registeredUser.isBannedByAdmin()),
+                // Se lanza la excepción
+                () -> assertThrows(
+                        ResourceBannedByAdministratorException.class,
+                        () -> userService.login(nickname, clearPassword)
+                )
+        );
+    }
+    
+    
+    @Test
+    void whenLoginWithID_thenUserDataIsReturned()
+            throws EntityAlreadyExistsException, ResourceBannedByAdministratorException, EntityNotFoundException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+    
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+        User loggedUser = userService.loginFromToken(registeredUser.getId());
+    
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Se puede recuperar los datos del usuario
+                () -> assertEquals(expectedUser.getNickname(), loggedUser.getNickname()),
+                () -> assertNotNull(expectedUser.getAvatar()),
+                () -> assertEquals(UserRole.USER, expectedUser.getRole())
+        );
+    }
+    
+    
+    @Test
+    void whenLoginWithIDNonExistentUser_thenRaiseEntityNotFoundException() {
+        // Comprobar resultados
+        assertAll(
+                // Se lanza la excepción
+                () -> assertThrows(
+                        EntityNotFoundException.class,
+                        () -> userService.loginFromToken(NON_EXISTENT_UUID)
+                )
+        );
+    }
+    
+    @Test
+    void whenUserIsBanned_andLoginWithID_thenRaiseResourceBannedByAdminException() throws EntityAlreadyExistsException {
+        // Crear datos de prueba
+        String nickname = "Foo";
+        User expectedUser = generateValidUser(nickname);
+        String clearPassword = expectedUser.getPassword();
+        expectedUser.setBannedByAdmin(true);
+        
+        // Ejecutar funcionalidades
+        User registeredUser = userService.signUp(expectedUser);
+        
+        // Comprobar resultados
+        assertAll(
+                // Se ha registrado un usuario
+                () -> assertNotNull(registeredUser),
+                // Usuario está baneado
+                () -> assertTrue(registeredUser.isBannedByAdmin()),
+                // Se lanza la excepción
+                () -> assertThrows(
+                        ResourceBannedByAdministratorException.class,
+                        () -> userService.loginFromToken(registeredUser.getId())
                 )
         );
     }
