3dcc190fce4699df995c2886b6f03cc326663d30
Javier Cancela Mato <javier.cmato@udc.es>
2022-07-07 12:29:48 +0200

    Implementados tests para cambiar contraseña

--- frontend-tfg/src/tests/Users/userActions.test.ts
+++ frontend-tfg/src/tests/Users/userActions.test.ts
@@ -11,6 +11,7 @@ const mockStore = createMockStore(middlewares);
 describe("User actions tests: ", () => {
     const signUpBackendSpy = jest.spyOn(userService, 'signUp');
     const loginBackendSpy = jest.spyOn(userService, 'login');
+    const changePasswordBackendSpy = jest.spyOn(userService, 'login');
 
 /* ******************** CONFIGURACIÓN PREVIA ******************** */
     afterEach(() => jest.restoreAllMocks());
@@ -172,6 +173,49 @@ describe("User actions tests: ", () => {
         /* ******************** COMPROBAR ******************** */
         //expect(loginBackendSpy.mock.calls[0][0]).toEqual(inputUserData.nickname);
         //expect(loginBackendSpy.mock.calls[0][1]).toEqual(inputUserData.password);
+        expect(store.getActions()).toEqual(expectedActions);
+    });
+
+    test('Change Password action -> SUCCESS', () => {
+        /* ******************** PREPARAR DATOS ******************** */
+        // Datos que introduce el usuario
+        let inputUserData = {
+            oldPassword: "oldPassword",
+            newPassword: "newPassword",
+        };
+        // Respuesta esperada por el backend
+        let result = {};
+        const onSuccess = jest.fn();
+        const onErrors = jest.fn();
+        const onReauthenticate = jest.fn();
+
+        // Reimplementación del backend para que se pasen por defecto los argumentos a testear
+        let changePasswordMockImplementation = (_oldPassword: string,
+                                                _newPassword: string,
+                                                _onSuccess: CallbackFunction,
+                                                _onErrors: CallbackFunction) => _onSuccess(result);
+        changePasswordBackendSpy.mockImplementation(changePasswordMockImplementation);
+
+        const action = userRedux.actions.changePasswordAsyncAction(
+            "00000000-0000-4000-8000-000000000000",
+            inputUserData.oldPassword,
+            inputUserData.newPassword,
+            onSuccess,
+            onErrors
+        );
+        const expectedActions = [
+            userRedux.actions.changePasswordAction()
+        ];
+
+
+        /* ******************** EJECUTAR ******************** */
+        const store = mockStore(initialState);
+        store.dispatch<any>(action);
+
+
+        /* ******************** COMPROBAR ******************** */
+        //expect(changePasswordBackendSpy.mock.calls[0][0]).toEqual(inputUserData.oldPassword);
+        //expect(changePasswordBackendSpy.mock.calls[0][1]).toEqual(inputUserData.newPassword);
         expect(store.getActions()).toEqual(expectedActions);
     });
 
