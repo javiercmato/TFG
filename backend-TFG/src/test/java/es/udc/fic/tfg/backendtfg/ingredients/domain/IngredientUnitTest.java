4513dd6883e1eded2faca0af1c277e3f010def38
Javier Cancela Mato <javier.cmato@udc.es>
2022-07-28 11:03:50 +0200

    Tests de unidad de entidades de ingredientes

---
+++ backend-TFG/src/test/java/es/udc/fic/tfg/backendtfg/ingredients/domain/IngredientUnitTest.java
@@ -0,0 +1,45 @@
+package es.udc.fic.tfg.backendtfg.ingredients.domain;
+
+import es.udc.fic.tfg.backendtfg.ingredients.domain.entities.Ingredient;
+import es.udc.fic.tfg.backendtfg.ingredients.domain.entities.IngredientType;
+import es.udc.fic.tfg.backendtfg.users.domain.entities.User;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class IngredientUnitTest {
+    
+    @Test
+    public void createIngredient() {
+        // Crear datos de prueba
+        UUID id = UUID.randomUUID();
+        String name = "ingredient";
+        IngredientType type = new IngredientType();
+        type.setId(UUID.randomUUID());
+        type.setName("IngredientType");
+        User creator = new User();
+        
+
+        // Ejecutar código
+        Ingredient ingredient = new Ingredient();
+        ingredient.setId(id);
+        ingredient.setName(name);
+        ingredient.setIngredientType(type);
+        ingredient.setCreator(creator);
+        
+        // Comprobar resultados
+        assertAll(
+                () -> assertEquals(id, ingredient.getId()),
+                () -> assertEquals(name, ingredient.getName()),
+                () -> assertEquals(type, ingredient.getIngredientType()),
+                () -> assertEquals(creator, ingredient.getCreator())
+        );
+    }
+}
