38dc9003834eb0c38b1733dc5126326445528f8b
Javier Cancela Mato <javier.cmato@udc.es>
2022-07-28 11:19:44 +0200

    Tests de unidad de entidades de usuario

---
+++ backend-TFG/src/test/java/es/udc/fic/tfg/backendtfg/users/domain/PrivateListUnitTest.java
@@ -0,0 +1,42 @@
+package es.udc.fic.tfg.backendtfg.users.domain;
+
+import es.udc.fic.tfg.backendtfg.users.domain.entities.PrivateList;
+import es.udc.fic.tfg.backendtfg.users.domain.entities.User;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PrivateListUnitTest {
+    
+    @Test
+    public void createPrivateList() {
+        // Crear datos de prueba
+        UUID id = UUID.randomUUID();
+        String title = "title";
+        String description = "My private list of recipes";
+        User creator = new User();
+        
+        // Ejecutar código
+        PrivateList privateList = new PrivateList();
+        privateList.setId(id);
+        privateList.setTitle(title);
+        privateList.setDescription(description);
+        privateList.setCreator(creator);
+        
+        // Comprobar resultados
+        assertAll(
+                () -> assertEquals(id, privateList.getId()),
+                () -> assertEquals(title, privateList.getTitle()),
+                () -> assertEquals(description, privateList.getDescription()),
+                () -> assertEquals(creator, privateList.getCreator())
+        );
+        
+    }
+}
