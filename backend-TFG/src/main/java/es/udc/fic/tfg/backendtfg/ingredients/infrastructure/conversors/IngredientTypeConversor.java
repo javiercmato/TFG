bbb2a8524b2358f7006797ca829d88f2cc429cf3
Javier Cancela Mato <javier.cmato@udc.es>
2022-07-28 17:36:29 +0200

    Implementado crear ingrediente en IngredientController

---
+++ backend-TFG/src/main/java/es/udc/fic/tfg/backendtfg/ingredients/infrastructure/conversors/IngredientTypeConversor.java
@@ -0,0 +1,15 @@
+package es.udc.fic.tfg.backendtfg.ingredients.infrastructure.conversors;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class IngredientTypeConversor {
+    /* ******************** Convertir a DTO ******************** */
+    
+    /* ******************** Convertir a conjunto de DTO ******************** */
+    
+    
+    /* ******************** Convertir a Entidad ******************** */
+    
+    
+}
